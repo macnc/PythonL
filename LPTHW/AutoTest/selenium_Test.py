a1b220e257c8633aa259b9a588ff62ba05459881
suntao <mac4st@me.com>
2016-01-12 20:14:39 +0800

    添加自动化测试脚本

---
+++ LPTHW/AutoTest/selenium_Test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# _*_coding:utf-8
+
+from selenium import webdriver
+import time
+
+dr = webdriver.Safari()
+time.sleep(5)
+print "Safari will be closed..."
+dr.quit()
+print "Safari has been closed!"
