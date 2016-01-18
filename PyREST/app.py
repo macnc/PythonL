5cd5774fff0fc9b6079c06b33355febad6aee410
suntao <mac4st@me.com>
2016-01-18 23:42:31 +0800

    Update RESTful API file with 404 errorhandler function which return json.

--- PyREST/app.py
+++ PyREST/app.py
@@ -1,7 +1,7 @@
 #!/flask/bin/python
 # _*_coding: utf-8
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response, abort
 
 app = Flask(__name__)
 
@@ -21,9 +21,24 @@ tasks = [
 
 ]
 
+# Task restAPI 实现第一版本
+'''
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
 def get_task():
     return jsonify({'tasks': tasks})
+'''
+
+# Task RESTful API 第二个版本
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = filter(lambda t: t['id'] == task_id, tasks)
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not Found'}), 404)
 
 if __name__ == '__main__':
     app.run(debug=True)
