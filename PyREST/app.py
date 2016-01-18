4795fdd413a34bab3e772bf15076a94b6b468b37
suntao <mac4st@me.com>
2016-01-19 03:48:58 +0800

    We have GET/PUT/POST/DELETE handling method with RESTful APIs

--- PyREST/app.py
+++ PyREST/app.py
@@ -1,7 +1,7 @@
 #!/flask/bin/python
 # _*_coding: utf-8
 
-from flask import Flask, jsonify, make_response, abort
+from flask import Flask, jsonify, make_response, abort, request
 
 app = Flask(__name__)
 
@@ -21,12 +21,10 @@ tasks = [
 
 ]
 
-# Task restAPI 实现第一版本
-'''
+# Tasks RESTful API 实现第一版本
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
-def get_task():
+def get_tasks():
     return jsonify({'tasks': tasks})
-'''
 
 # Task RESTful API 第二个版本
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
@@ -36,9 +34,53 @@ def get_task(task_id):
         abort(404)
     return jsonify({'task': task[0]})
 
+# 数据找不到时的错误处理和返回值输出函数
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not Found'}), 404)
+
+# 为Tasks列表中添加task任务数据函数
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+# 更新task数据
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = filter(lambda t: t['id'] == task_id, tasks)
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not unicode:
+        abort(400)
+    if 'done' in request.json and type('done') is bool:
+        abort(400)
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+
+    return jsonify({'task': task[0]})
+
+# 删除Task数据RESTful API
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = filter(lambda t: t['id'] == task_id, tasks)
+    if len(task) == 0:
+        abort(404)
+    tasks.remove(task[0])
+    return jsonify({'result': True})
 
 if __name__ == '__main__':
     app.run(debug=True)
