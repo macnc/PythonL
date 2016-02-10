bdef9aac4ff6638666678fbe3f9f5d71a80d1e24
suntao <aoxilis@gmail.com>
2016-02-10 18:42:25 +0800

    Update the Ex34 files for the <Learn Python The Hard Way> Update the workspace files of the IDE Parameters.

--- LPTHW/Ex34/Ex34.py
+++ LPTHW/Ex34/Ex34.py
@@ -1,3 +1,20 @@
 #!/usr/bin/python
 # _*_coding: utf-8
 
+'''
+How to access elements of Lists?
+I will create a function which accept arguments and return the result from function.
+ '''
+
+import os
+
+animals = ['bear', 'tiger', 'penguin', 'zebra']
+
+def what_animals(index):
+    if index <= len(animals):
+        print 'Your answer is: ' + animals[index]
+    else:
+        print 'There are no animals like this here!'
+
+which = int(raw_input('Which one in animals do you want to know? >'))
+what_animals(which)
