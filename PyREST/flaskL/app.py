7f74bbbf815e61e0018786cf72a03806b144ecfd
suntao <aoxilis@gmail.com>
2016-04-22 17:42:07 +0800

    EP8 format coding

--- PyREST/flaskL/app.py
+++ PyREST/flaskL/app.py
@@ -2,11 +2,14 @@
 # _*_coding: utf-8
 
 from flask import Flask
+
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello():
     return "hello 孙涛!"
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
