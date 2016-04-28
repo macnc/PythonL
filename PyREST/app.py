3d20757aec3b30da5e0245b9e028e19838cc103d
suntao <aoxilis@gmail.com>
2016-04-28 11:35:15 +0800

    修改项目的目录结构

---
+++ PyREST/app.py
@@ -0,0 +1,256 @@
+#!~/anaconda2/bin/python
+# _*_coding: utf-8
+
+from flask import Flask, jsonify, make_response, abort, request, url_for
+from flask_httpauth import HTTPBasicAuth
+from flask_restful import fields, marshal, Resource, reqparse, Api
+from passlib.apps import custom_app_context as pwd_context
+import MySQLdb as db
+
+db_config = {
+    'host': '127.0.0.1',
+    'user': 'root',
+    'passwd': 'LoveDesign**!',
+    'db': 'Flask_RESTful'
+}
+
+con = db.connect(**db_config)
+
+auth = HTTPBasicAuth()
+app = Flask(__name__)
+api = Api(app)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+
+]
+
+task_fields = {
+    'title': fields.String,
+    'description': fields.String,
+    'done': fields.Boolean,
+    'uri': fields.Url('task')
+
+}
+
+
+class User(db):
+
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), index=True)
+    password_hash = db.Column(db.String(128))
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+
+# 需要鉴权使用的用户信息API
+class UserAPI(Resource):
+    def get(self, id):
+        pass
+
+    def put(self, id):
+        pass
+
+    def delete(self, id):
+        pass
+
+
+api.add_resource(UserAPI, '/users/<int:id>', endpoint='user')
+
+
+# 处理任务列表逻辑的API
+class TaskListAPI(Resource):
+
+    decorators = [auth.login_required]
+
+    def __int__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('title', type=str, required=True,
+                                   help='no task title provided',
+                                   location='json')
+        self.reqparse.add_argument('description', type=str,
+                                   default="", location='json')
+        super(TaskListAPI, self).__init__()
+
+    def get(self):
+        pass
+
+    # def put(self):
+    #     pass
+
+    def post(self):
+        pass
+
+
+# 处理单项任务逻辑的API
+class TaskAPI(Resource):
+
+    decorators = [auth.login_required]
+
+    def __int__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('title', type=str, location='json')
+        self.reqparse.add_argument('description', type=str, location='json')
+        self.reqparse.add_argument('done', type=bool, location='json')
+        super(TaskAPI, self).__init__()
+
+    def get(self):
+        pass
+
+    def put(self, id):
+        task = filter(lambda t: t['id'] == id, tasks)
+        if len(task) == 0:
+            abort(404)
+        task = task[0]
+        args = self.reqparse.parse_args()
+        for k, v in args.iteritems():
+            if v is not None:
+                task[k] = v
+
+        return {'task': marshal(task, task_fields)}
+
+    def delete(self):
+        pass
+
+api.add_resource(TaskListAPI, '/todo/api/v1.0/tasks', endpoint='tasks')
+api.add_resource(TaskAPI, '/todo/api/v1.0/tasks/<int:id>', endpoint='tasks')
+
+
+'''目前 API 的设计的问题就是迫使客户端在任务标识返回后去构造 URIs。这对于服务器是十分简单的，
+但是间接地迫使客户端知道这些 URIs 是如何构造的.
+这将会阻碍我们以后变更这些 URIs。不直接返回任务的 ids，我们直接返回控制这些任务的完整的 URI，以便客户端可以随时使用这些 URIs。
+为此，我们可以写一个小的辅助函数生成一个 “公共” 版本任务发送到客户端:'''
+
+
+def make_public_task(task):
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_task', task_id=task['id'],
+                                      _external=True)
+        else:
+            new_task[field] = task[field]
+    return new_task
+
+
+@app.route('/api/users', method=['POST'])
+def new_user():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        abort(400)  # Missing arguments
+    if User.query.filter_by(username=username).first is not None:
+        abort(400)  # Existing user
+
+    user = User(username=username)
+    user.hash_password(password)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({'username': user.username}), 201, {'Location':
+                                                       url_for('get_user',
+                                                               id=user.id,
+                                                               _external=True)
+                                                       }
+
+
+# Tasks RESTful API 实现第一版本
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@auth.login_required
+def get_tasks():
+    return jsonify({'tasks': map(make_public_task, tasks)})
+
+
+# Task RESTful API 第二个版本
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = filter(lambda t: t['id'] == task_id, tasks)
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+
+# 数据找不到时的错误处理和返回值输出函数
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not Found'}), 404)
+
+
+# 为Tasks列表中添加task任务数据函数
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or 'title'not in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': make_public_task(task[0])})
+
+
+# 更新task数据
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+@auth.login_required
+def update_task(task_id):
+    task = filter(lambda t: t['id'] == task_id, tasks)
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not unicode:
+        abort(400)
+    if 'done' in request.json and type('done') is bool:
+        abort(400)
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description',
+                                              task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+
+    return jsonify({'task': task[0]})
+
+
+# 删除Task数据RESTful API
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = filter(lambda t: t['id'] == task_id, tasks)
+    if len(task) == 0:
+        abort(404)
+    tasks.remove(task[0])
+    return jsonify({'result': True})
+
+
+@auth.get_password
+def get_password(username):
+    if username == 'miguel':
+        return 'python'
+    return None
+
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 403)
+
+
+# 主函数入口
+if __name__ == '__main__':
+    app.run(debug=True)
