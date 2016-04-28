d99b4e90841bf985cc8eb7d12e4e9489365b7ca3
suntao <aoxilis@gmail.com>
2016-04-28 15:47:31 +0800

    Update the virtualen for the project

--- PyREST/Hello.py
+++ PyREST/Hello.py
@@ -2,12 +2,12 @@
 # _*_coding: utf-8
 
 from flask import Flask
-
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    return "Hello Flask!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8660)
---
+++ PyREST/models.py
@@ -0,0 +1,4 @@
+#! /virtualenvs/py_environment/bin/python2.7
+# _*_coding: utf-8
+
+import sqlalchemy as db
