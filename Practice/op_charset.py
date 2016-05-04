133ab1745824f6df1a8e451faa0152d28e9e57bf
suntao <mac4st@me.com>
2016-05-04 13:20:11 +0800

    change the number of charset

--- Practice/op_charset.py
+++ Practice/op_charset.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python
 # _*_coding: utf-8
 
-charset = 'abcdefghijklmnopqrstuvwxyz' * 2
+charset = 'abcdefghijklmnopqrstuvwxyz' * 2 + 'i'
 for i in range(0, len(charset)):
     if i > len(charset)/2:
         break;
