3fdc985c6bf4124b03563351c78a77bbb83bf8b6
孙涛 <aoxilis@gmail.com>
2016-05-07 17:16:58 +0800

    5.0: 数据库ORM对象创建和关系建立 - 5.8: 数据库操作；创建表，插入行，修改行，删除行，查询行

--- PyREST/Flask_dev/hello.py
+++ PyREST/Flask_dev/hello.py
@@ -15,17 +15,42 @@ from flask.ext.script import Manager
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 manager = Manager(app)
 moment = Moment(app)
 bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:LoveDesign**@localhost/flask_dev'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+db = SQLAlchemy(app)
 
 
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+	return '<User %r>' % self.username
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
