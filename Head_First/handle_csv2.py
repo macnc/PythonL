11d7b17f431c896bb67aed45dd83fbac48a358bc
suntao <mac4st@me.com>
2016-06-09 00:31:14 +0800

    Add new file for handling the csv file

---
+++ Head_First/handle_csv2.py
@@ -0,0 +1,16 @@
+#! /usr/local/bin/python3
+# _*_coding: utf-8
+
+
+row_data = {}
+with open('PaceData.csv') as data:
+	column_heading = data.readline().strip().split(',')
+	column_heading.pop(0)
+
+	for each_line in data:
+		row = each_line.strip().split(',')
+		row_label = row(0)
+		inner_dict = {}
+		for i in range(len(column_heading)):
+			inner_dict[row[i]] = column_heading[i]
+		row_data[row_label] = inner_dict
